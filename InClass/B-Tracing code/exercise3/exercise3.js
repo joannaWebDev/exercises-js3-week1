a367f76ac897e00a0f204ced405a237256b4150f
joannaWebDev <joannasmerea@gmail.com>
2020-10-06 22:11:36 +0200

    finished B & C

--- InClass/B-Tracing code/exercise3/exercise3.js
+++ InClass/B-Tracing code/exercise3/exercise3.js
@@ -1,24 +1,19 @@
-const getRepos = function(repoName) {
-  return fetch('https://api.github.com/users/' + repoName + '/repos')  
-    .then(data => data.json())
-    .then(function(response) {
-      return response.map(function(rep) {
-		console.log(rep.name);
-        return rep.name;
-      });
-    });
+const getRepos = repoName => {
+  return fetch("https://api.github.com/users/" + repoName + "/repos").then(res => onReposReady(res));
 };
-
-
-
-const migracodeRepos = getRepos('migracode-barcelona');
-console.log("Loading...");
-
-
-var myElement = document.querySelector("#main");
-var paragraph = document.createElement("p"); 
-paragraph.innerText = migracodeRepos;
-myElement.appendChild(paragraph);
+const onReposReady = data => {
+  data.json().then(repos => {
+    repos.map(repo => {
+      let p = document.createElement("p");
+      p.innerText = repo.name;
+      myElement.appendChild(p);
+    });
+  });
+};
+getRepos("migracode-barcelona");
+const myElement = document.querySelector("#main");
+//Task1. Fix the code
+//Task2. Create a <p> for each repository
 
 //Task1. Fix the code
 //Task2. Create a <p> for each repository
