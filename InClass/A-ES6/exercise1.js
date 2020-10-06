ea54b2d58bc0c1d05b662331567440f71bcc5eda
joannaWebDev <joannasmerea@gmail.com>
2020-10-06 12:01:03 +0200

    A finished

--- InClass/A-ES6/exercise1.js
+++ InClass/A-ES6/exercise1.js
@@ -2,24 +2,30 @@
 function arrow() {
   return "es6 is awesome!";
 }
+const arrow = () => 'es6 is awesome!';
 
 //Use the es6 syntax for default parameter
 function defaultParameter(name) {
   var name = name || "sam";
   return name;
 }
+const defaultParameter = (name = 'sam ') =>  name;
 
 // Use the spread operator to combine arr1 and arr2
 function combineArrays(arr1, arr2) {
   return arr1.concat(arr2);
 }
-
+const combineArrays = (arr1, arr2) => [...arr1,...arr2];
 //use destructuring to return the object's cyf property
 function destructuring(obj) {
   return obj.cyf;
 }
-
+const destructuring = (obj) => {
+  const {cyf} = obj;
+  return cyf;
+}
 // use template literal to return a string with the sum of a and b
 function templateString(a, b) {
   return "The sum is equal to " + (a + b).toString();
 }
+const templateString = (a, b) => `The sum is equal to ${(a + b).toString()}`;
--- InClass/A-ES6/exercise2.js
+++ InClass/A-ES6/exercise2.js
@@ -1,8 +1,10 @@
+//clase base
 class Polygon {
   constructor(height, width) {
     this.name = "Polygon";
     this.height = height;
     this.width = width;
+    //this.area = this.height * this.width;
   }
   sayName() {
     console.log('Hi, I am a ' + this.name);
@@ -13,7 +15,8 @@ let p = new Polygon('Polygon', 300, 400);
 p.sayName();
 console.log('The area of this polygon is ' + p.area);
 
-
+//herencia
+//palabras claves: 'extends', 'super'
 class Rectangle extends Polygon {
   constructor(height, width) {
     super(height, width);
@@ -23,7 +26,7 @@ class Rectangle extends Polygon {
   }
   get area() {return this.width * this.height;}
   sayName() {
-    console.log('Hi I am a polygon and my name is ' + this.name + '.');
+    console.log('Hi I am not a polygon and my name is ' + this.name + '.');
   }
 }
 
@@ -33,7 +36,45 @@ r.sayName();
 console.log('The area of this polygon is ' + r.area);
 
 //Create 2 new classes that extend from polygon: triangle and circle and log their names and areas
+class Triangle extends Polygon{
+  constructor(height, width){
+  super(height, width);
+  this.name="triangle";
+  this.area=(width * height) / 2;
+ 
+  }
+ get areaResult(){
+  return this.area
+  }
+}
 
+let newTriangle=new Triangle(100, 200)
+console.log(`${newTriangle.name}  ${newTriangle.areaResult}`);
 
+//
+class Trapezoid extends Polygon{
+  constructor(height, base1, base2){
+  super(height)
+  this.base1=base1
+  this.base2=base2
+  this.name="Trapezoid"
+  this.area=(this.base1 + this.base2) * this.height / 2
+}}
 
+let newTrpzd=new Trapezoid(100, 100, 100)
+console.log(`${newTrpzd.name} ${newTrpzd.area}`)
 
+//
+class Circle extends Polygon {
+  constructor(radio) {
+    super(radio, radio);
+    this.name = 'Circle';
+    this.sides = 0;
+  }
+  get area() {
+    return Math.PI * Math.pow(this.width, 2) ;
+  }
+}
+
+let circle = new Polygon(5)
+console.log(`${Circle.name} ${Circle.area}`)
