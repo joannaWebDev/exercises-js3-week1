a2578412497eb765cd535a613c34682cda5d535d
joannaWebDev <joannasmerea@gmail.com>
2020-10-10 10:50:34 +0200

    finished inclass

--- InClass/B-Tracing code/exercise1/exercise1.js
+++ InClass/B-Tracing code/exercise1/exercise1.js
@@ -1,19 +1,32 @@
-function greetPeople(people) {
-  var greeting = "He";
+const greetPeople = (people) => {
+  let greeting = "He";
   greeting = greeting + "llo";
   greeting = greeting + " ";
 
-  people.forEach(function(person) {
-    greeting = greeting + person;
-	console.log(greeting);
+
+  people.forEach((person) => {
+    //greeting = greeting + person;
+    greeting = `${greeting} ${person} ,`;
+	  console.log(greeting);
   });
 
-  return greeting;
+
+for (let i = 0; i < people.length; i++) {
+  if (i === people.length - 2) {
+    greeting = `${greeting} ${people[i]} and`;     
+  } else if (i === people.length - 1){
+    greeting = `${greeting} ${people[i]}.`;        
+  }
+  else {
+    greeting = `${greeting} ${people[i]} `;  
+  }
 }
+  console.log(greeting);
+  return greeting;
+};
 
 /*
-  Let's trace this piece of code - what is the value of result with this input
-  
+  Let's trace this piece of code - what is the value of result with this input  
   */
-  var mentors = ['Irina', 'Ashleigh', 'Etza'];
-  var result = greetPeople(mentors)
+  const mentors = ['Irina', 'Ashleigh', 'Etza'];
+  const result = greetPeople(mentors)
--- InClass/B-Tracing code/exercise2/exercise2.js
+++ InClass/B-Tracing code/exercise2/exercise2.js
@@ -1,44 +1,25 @@
-function removeVowels(word) {
-  var characters = word.split("");
-
-  var result = [];
-
-  characters.forEach(function(character) {
-    if (
-      character === "a" ||
-      character === "o" ||
-      character === "i" ||
-      character === "e" ||
-      character === "u"
-    ) {
+const removeVowels = word => {
+  const characters = word.split("");
+  const result = [];
+  characters.forEach(character => {
+    if (character === "a" || character === "o" || character === "i" || character === "e" || character === "u") {
       result.push(character);
     } else {
       result.push("_");
     }
   });
-
   return result.join("");
-}
-
-function removeVowelsForWords(words) {
-  var result = words.map(function(word) {
-    return removeVowels(word);
-  });
-
-  return result;
-}
-
+};
+const removeVowelsForWords = words => words.map(word => removeVowels(word));
 /*
   Task 1
   Let's trace this piece of code - what is the value of result with this input
   */
-  var result = removeVowels('samuel');
-
-
-
+const result = removeVowels("samuel");
 /*
 	Task 2
 	Trace what happens if we call the function removeVowelsForWords with this input: ["Irina", "Etza", "Daniel"]
     expected output?:
 */
-
+const resultFromWords = removeVowelsForWords(["Irina", "Etza", "Daniel"]);
+console.log(resultFromWords);
